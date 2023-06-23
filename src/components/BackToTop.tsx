53c073f333a14cb85252fda7a03d44561235200d
Allison <a.velardep@pucp.edu.pe>
2023-06-23 11:47:53 -0500

    Add back to top button

---
+++ src/components/BackToTop.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { BsArrowUpCircleFill } from "react-icons/bs";
+
+const BackToTop = () => {
+  const [backToTopButton, setBackToTopButton] = useState<boolean>(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setBackToTopButton(true);
+      } else {
+        setBackToTopButton(false);
+      }
+    });
+  }, []);
+
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <button
+      className="sticky bottom-5 opacity-75 text-yellow-500 ml-auto animate-fade animate-once animate-ease-out"
+      onClick={scrollUp}
+    >
+      {backToTopButton && <BsArrowUpCircleFill size={40} />}
+    </button>
+  );
+};
+
+export default BackToTop;
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { ToastContainer } from "react-toastify";
-import { BsArrowUpCircleFill } from "react-icons/bs";
 import "react-toastify/dist/ReactToastify.css";
 
 import UserGenerator from "@/modules/User/UserGenerator";
 import Navbar from "@/components/Navbar";
+import BackToTop from "@/components/BackToTop";
 
 const HomePage = () => {
   return (
@@ -13,10 +13,7 @@ const HomePage = () => {
       <div className="h-full py-8 grid grid-rows-layout justify-items-center gap-10 font-primary mobile:p-4 mobile:py-6 desktop:w-11/12 desktop:m-auto tablet:w-10/12 tablet:m-auto">
         <Navbar />
         <UserGenerator />
-        <BsArrowUpCircleFill
-          className="sticky bottom-5 right-2 opacity-75 text-yellow-500 ml-auto"
-          size={40}
-        />
+        <BackToTop />
       </div>
     </>
   );
