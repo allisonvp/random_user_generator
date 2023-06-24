09f3841a7627d8cb1dddc0f44d4cbcbc55fbc872
Allison <a.velardep@pucp.edu.pe>
2023-06-24 16:36:50 -0500

    fix home page

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -22,13 +22,12 @@ const BackToTop = () => {
   };
 
   return (
-    <></>
-    // <button
-    //   className="absolute bottom-5 opacity-75 text-brown-400 ml-auto animate-fade animate-once animate-ease-out"
-    //   onClick={scrollUp}
-    // >
-    //   {backToTopButton && <BsArrowUpCircleFill size={40} />}
-    // </button>
+    <button
+      className="absolute bottom-5 opacity-75 text-brown-400 ml-auto animate-fade animate-once animate-ease-out"
+      onClick={scrollUp}
+    >
+      {backToTopButton && <BsArrowUpCircleFill size={40} />}
+    </button>
   );
 };
 
