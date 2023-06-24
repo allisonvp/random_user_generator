24a1e3c5a74007bb0eacad3a10107447bb340407
Allison <a.velardep@pucp.edu.pe>
2023-06-24 16:53:36 -0500

    fix home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="w-full mobile:text-lg laptop:text-4xl tablet:text-3xl text-6xl text-center flex flex-col mobile:items-center tablet:items-center">
+    <div className="w-full mobile:text-lg laptop:text-4xl tablet:text-3xl text-5xl text-center flex flex-col mobile:items-center tablet:items-center">
       <div className="flex flex-col items-center justify-center gap-8 mobile:gap-4 h-1/2">
         <h1
           className="text-brown-500 animate-fade-right animate-once animate-duration-[3000ms] animate-delay-200 animate-ease-linear
@@ -25,24 +25,24 @@ const Home = () => {
         </h1>
       </div>
       <div className="flex justify-between h-1/2">
-        <div className="animate-fade-up animate-once animate-duration-[800ms] animate-ease-in-out tablet:hidden mobile:hidden mt-auto laptop:w-[400px] desktop:w-[420px]">
-          <Image
-            src="/Images/people.svg"
-            width={0}
-            height={0}
-            sizes="100vw"
-            alt="people talking"
-            style={{ width: "100%", height: "auto" }}
-          ></Image>
-        </div>
-        <div className="animate-fade-up animate-once animate-duration-[800ms] animate-ease-in-out mt-auto tablet:w-[350px] laptop:w-[400px] desktop:w-[420px]">
+        <div className="animate-fade-up animate-once animate-duration-[800ms] animate-ease-in-out mt-auto h-full">
           <Image
             src="/Images/people-2.svg"
             width={0}
             height={0}
             sizes="100vw"
             alt="people talking"
-            style={{ width: "100%", height: "auto" }}
+            style={{ width: "100%", height: "100%" }}
+          ></Image>
+        </div>
+        <div className="animate-fade-up animate-once animate-duration-[800ms] animate-ease-in-out tablet:hidden mobile:hidden mt-auto h-full">
+          <Image
+            src="/Images/people.svg"
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="people talking"
+            style={{ width: "100%", height: "100%" }}
           ></Image>
         </div>
       </div>
