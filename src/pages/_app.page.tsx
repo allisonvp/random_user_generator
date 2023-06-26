d610a073cca6fd61127dde6b28b7499131101802
Allison <a.velardep@pucp.edu.pe>
2023-06-26 10:04:21 -0500

    update metadata

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -21,6 +21,16 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
     <SnackbarProvider maxSnack={3} autoHideDuration={4000}>
       <Head>
         <title>Random Users Generator</title>
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="This is a random user generator to meet new people"
+        />
+        <meta name="keywords" content="random users, user generator" />
+        <meta name="author" content="Allison Velarde"></meta>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:description" content="random user generator" />
       </Head>
       {getLayout(<Component {...pageProps} />)}
     </SnackbarProvider>
